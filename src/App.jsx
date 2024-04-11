e68f56c18b50bba45446d2fa77980f66fa592928
Andre1717g <vv18009@ues.edu.sv>
2024-04-11 09:27:06 -0600

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Header from './components/Header'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <div>
+      <Header /> {/* Renderiza el componente de encabezado */}
+      <div className="container mx-auto">
+        
+      </div>
+    </div>
       <div className="card card-compact w-96 bg-base-100 shadow-xl">
         <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
         <div className="card-body">
