5cf486376e13a6f3795b3a60d8d44d39f708864c
Wilmer Garcia <104747642+ZequiGarcia@users.noreply.github.com>
2024-04-18 21:33:16 -0600

    Add files via upload

---
+++ src/products/page/ProductPage.jsx
@@ -0,0 +1,30 @@
+
+import React, { useState } from 'react';
+import { useAllProduct, useFilter } from '../../hooks';
+import { Filter, Product } from '../components';
+
+export const ProductPage = () => {
+    const { productos } = useAllProduct(); 
+    const { filteredProducts, filtrarProductosPorCategoria } = useFilter(); 
+    const [filtered, setFiltered] = useState(false);
+
+    const handleFilter = (categoria) => {
+        filtrarProductosPorCategoria(categoria); 
+        setFiltered(true);
+        console.log(filteredProducts);
+    };
+
+    // Esta función se encargará de restablecer el filtro
+    const handleResetFilter = () => {
+        filtrarProductosPorCategoria(''); 
+        setFiltered(false);
+    console.log(filteredProducts);
+    };
+
+    return (
+        <>
+            <Filter onFilter={handleFilter} onReset={handleResetFilter} />
+            <Product productos={filtered ? filteredProducts : productos} /> {/* Pasamos los productos filtrados si se aplicó un filtro, de lo contrario, pasamos todos los productos */}
+        </>
+    );
+};
