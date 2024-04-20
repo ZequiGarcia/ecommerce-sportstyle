e3e4a4ac96f75bd5d1ccccb1e3fa83ac7270bde7
Andre1717g <vv18009@ues.edu.sv>
2024-04-20 09:35:18 -0600

    agg de nav

--- src/products/components/Header.jsx
+++ src/products/components/Header.jsx
@@ -1,17 +1,29 @@
 import logo from '../../images/logoss.png'; // Importa la imagen
+import { Link } from 'react-router-dom'; // Importa Link si estás utilizando React Router
 
 export const Header = () => {
-    return(
-        <div className="navbar bg-base-100  bg-neutral">
-            <div className="flex-1">
-            <img src={logo} alt="Logo" className="h-20 me-3" /> {/* Mostramos el logo */}
-            </div>
+    return (
+        <div className="navbar bg-base-100 bg-neutral">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="flex-1">
+                    <img src={logo} alt="Logo" className="h-20" /> {/* Muestra el logo */}
+                </div>
 
-            <div className="flex-none">
-                <div className="dropdown dropdown-end">
+                <div className="flex-none">
+                <ul className="flex space-x-24 text-neutral-content justify-center">
+                    <li><Link to="/" className="hover:text-gray-300 text-lg font-bold">Inicio</Link></li>
+                    <li><Link to="/tienda" className="hover:text-gray-300 text-lg font-bold">Tienda</Link></li>
+                    <li><Link to="/marca" className="hover:text-gray-300 text-lg font-bold">Marca</Link></li>
+                    <li><Link to="/oferta" className="hover:text-gray-300 text-lg font-bold">Oferta</Link></li>
+                    <li><Link to="/categoria" className="hover:text-gray-300 text-lg font-bold">Categoría</Link></li>
+                </ul>
+
+                </div>
+
+                <div className="dropdown dropdown-end ml-20 mr-6">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle bg-white">
                         <div className="indicator">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0 2 2 0 01-4 0z" /></svg>
                             <span className="badge badge-sm indicator-item ">8</span>
                         </div>
                     </div>
@@ -34,6 +46,6 @@ export const Header = () => {
                     </div>
                 </div>
             </div>
-            </div>
-    )
-}
+        </div>
+    );
+};
