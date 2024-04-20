4d41d834d8599ee8624ed200ab921d947f43cef1
DIego Umaña <55554diego@gmail.com>
2024-04-20 12:48:31 -0600

    Login diseno

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,7 +13,7 @@ function Login({ setAuth }) {
 
     if (registeredUser && registeredUser.user === user && registeredUser.password === password) {
       setAuth(true);
-      navigate('/'); // Redirige al usuario a la página principal después del inicio de sesión exitoso
+      navigate('/'); 
     } else {
       Swal.fire({
         icon: 'error',
@@ -23,18 +23,17 @@ function Login({ setAuth }) {
     }
   };
 
-
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <form onSubmit={handleSubmit} className="p-10 bg-white rounded flex justify-center items-center flex-col shadow-md">
-        <p className="mb-5 text-3xl uppercase text-gray-600">Inicio de sesión</p>
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <form onSubmit={handleSubmit} className="p-10 bg-gray-800 rounded-lg flex justify-center items-center flex-col shadow-xl">
+        <p className="mb-5 text-3xl uppercase text-white">Inicio de sesión</p>
         <input
           type="text"
           name="user"
           value={user}
           onChange={(e) => setUser(e.target.value)}
           placeholder="Usuario"
-          className="mb-5 p-3 w-80 border-2 border-gray-300"
+          className="mb-5 p-3 w-80 border rounded-lg border-gray-700 bg-gray-700 text-white"
           required
         />
         <input
@@ -43,10 +42,10 @@ function Login({ setAuth }) {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Contraseña"
-          className="mb-5 p-3 w-80 border-2 border-gray-300"
+          className="mb-5 p-3 w-80 border rounded-lg border-gray-700 bg-gray-700 text-white"
           required
         />
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button type="submit" className="w-full bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-lg transition duration-300">
           Iniciar Sesión
         </button>
       </form>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,36 +3,37 @@ import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
 function Register() {
-    const [user, setUser] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newUser = { user, password };
   
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const newUser = { user, password };
-      // Aquí podrías agregar validaciones adicionales para el usuario y la contraseña
-      localStorage.setItem('user', JSON.stringify(newUser));
-      Swal.fire({
-        icon: 'success',
-        title: '¡Registro exitoso!',
-        text: 'Ahora puedes iniciar sesión con tus credenciales.',
-      }).then((result) => {
-        if (result.isConfirmed) {
-          navigate('/login'); // Redirige al usuario a la página de inicio de sesión después del registro
-        }
-      });
-    };
+    localStorage.setItem('user', JSON.stringify(newUser));
+    Swal.fire({
+      icon: 'success',
+      title: '¡Registro exitoso!',
+      text: 'Ahora puedes iniciar sesión con tus credenciales.',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        navigate('/login');
+      }
+    });
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <form onSubmit={handleSubmit} className="p-10 bg-white rounded flex justify-center items-center flex-col shadow-md">
-        <p className="mb-5 text-3xl uppercase text-gray-600">Registro</p>
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <form onSubmit={handleSubmit} className="p-10 bg-gray-800 rounded-lg flex justify-center items-center flex-col shadow-xl">
+        <p className="mb-5 text-3xl uppercase text-white">Registro</p>
         <input
           type="text"
           name="user"
           value={user}
           onChange={(e) => setUser(e.target.value)}
           placeholder="Usuario"
-          className="mb-5 p-3 w-80 border-2 border-gray-300"
+          className="mb-5 p-3 w-80 border rounded-lg border-gray-700 bg-gray-700 text-white"
           required
         />
         <input
@@ -41,10 +42,10 @@ function Register() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Contraseña"
-          className="mb-5 p-3 w-80 border-2 border-gray-300"
+          className="mb-5 p-3 w-80 border rounded-lg border-gray-700 bg-gray-700 text-white"
           required
         />
-        <button type="submit" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+        <button type="submit" className="w-full bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-lg transition duration-300">
           Registrarse
         </button>
       </form>
